f111ca68f5a570e21c7440a5819f91779ab3a436
im-nemessis <powerfulltech15@gmail.com>
2020-04-21 13:18:28 +0530

    made an fully responsive calculator app ui using html and css

---
+++ day 9/app.js
@@ -0,0 +1,19 @@
+const value = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four4: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  zero: 0,
+};
+
+function getEl(id) {
+  return document.querySelector(`${id}`);
+}
+
+ans = getEl(.ans);
+console.log('ans')
