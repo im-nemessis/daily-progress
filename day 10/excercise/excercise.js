2cd1007483429f02da7224d2923550ba7f81de9d
im-nemessis <powerfulltech15@gmail.com>
2020-04-23 12:59:54 +0530

    started javascript book eloquent javascript 3rd edition

---
+++ day 10/excercise/excercise.js
@@ -0,0 +1,9 @@
+//looping a triangle
+
+let i = 7;
+while (i > 0) {
+  let ans = "#######";
+  console.log(ans);
+  ans = ans.pop;
+  i--;
+}
---
+++ day11/excercise.js
@@ -0,0 +1,91 @@
+//Looping triangle
+
+var ans = "#";
+for (let i = 0; i < 7; i++) {
+  console.log(ans);
+  ans = ans + "#";
+}
+
+// fizzBuzz
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+  if (i % 5 === 0) {
+    console.log("buzz");
+  }
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("FizzBuzz");
+  } else {
+    console.log(i);
+  }
+}
+
+//chessboard
+
+function chessboard(num) {
+  for (let i = 1; i < num; i++) {
+    if (i % 2 === 0) {
+      let ans = "# ";
+      for (let i = 1; i < num / 2; i++) {
+        ans += "# ";
+      }
+      console.log(ans);
+    }
+    if (i % 2 !== 0) {
+      let ans = " #";
+      for (let i = 0; i < num / 2 - 0.5; i++) {
+        ans += " #";
+      }
+      console.log(ans);
+    }
+  }
+}
+
+chessboard(8);
+
+//minimum
+function min(a, b) {
+  if (b == undefined) {
+    return a;
+  }
+
+  if (a > b) {
+    return b;
+  } else {
+    return b;
+  }
+}
+
+console.log(min(2, 4));
+
+//bean counting
+
+function countsB(string) {
+  let count = 0;
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (string[i] === "B") {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countsB("BooB"));
+
+//char count
+
+function charCount(string, char) {
+  let count = 0;
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (string[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(charCount("boob", "o"));
