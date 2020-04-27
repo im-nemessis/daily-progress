c7f4bdce4ccbd4fc850c7b4dfd273598667e66cd
im-nemessis <powerfulltech15@gmail.com>
2020-04-27 14:24:45 +0530

    thoery note making

--- day14/ex.js
+++ day14/ex.js
@@ -1,27 +1,26 @@
 function deepComparison(obj1, obj2) {
   let bool = typeof obj1 == "object" && typeof obj1 != null;
 
-  console.log(bool);
-
   let bool2 = typeof obj2 == "object" && typeof obj2 != null;
 
-  console.log(bool2);
+  if (!bool && !bool2) {
+    if (!obj1 == null && !obj2 == null) {
+      if (obj1 === obj2) {
+        return true;
+      }
+    }
+  }
 
   if (bool && bool2) {
     let boolean = Object.keys(obj1).length == Object.keys(obj2).length;
 
-    console.log("bool : ", boolean);
-
     if (boolean) {
       let prop1 = Object.keys(obj1);
-      console.log(prop1);
 
       let prop2 = Object.keys(obj2);
 
-      console.log(prop2);
-
       let ans = "";
-      for (let i = 0; i < prop1.length, i++; ) {
+      for (let i = 0; i < prop1.length i++; ) {
         if (prop1[i] == prop2[i]) {
           ans = "isTrue";
         } else {
