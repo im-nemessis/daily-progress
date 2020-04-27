fe6c6784b7815a4ea0ada944fa067dfdbc2fd02e
im-nemessis <powerfulltech15@gmail.com>
2020-04-27 15:47:55 +0530

    comple

--- day15/index.js
+++ day15/index.js
@@ -3,3 +3,33 @@
 // build on each other, like a wall of mini stones.”
 
 //function
+
+//objective
+
+/*
+
+
+write a funtion which prints two number : the number of cows and the number of chiken in a farm with their name and the number before with a padding of zeros of three;
+
+
+*/
+
+function padZeros(number, width) {
+  let padded = `${number}`;
+  while (padded.length < width) {
+    padded = "0" + padded;
+  }
+  return padded;
+}
+
+function countOf(n, objInFarm) {
+  return `${n} ${objInFarm}`;
+}
+
+function printFarmInventory(num, ...objs) {
+  for (obj of objs) {
+    console.log(countOf(padZeros(num, 7), obj));
+  }
+}
+
+printFarmInventory(10, "hen");
